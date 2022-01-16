fb485f26d7855a7578399267df72e965c67a86c7
jongeluijs <59418954+jongeluijs@users.noreply.github.com>
2022-01-16 21:23:03 +0100

    typos

--- AirQuality.py
+++ AirQuality.py
@@ -9,19 +9,19 @@ class Database():
         self.cur = self.conn.cursor()
 
     def add_enviro(self, time, temp, pres, hum, light, prox, noise):
-        self.cur.execute("INSERT INTO environ(timestamp, temperature, pressure, humidity, light, proximity, noise) 
+        self.cur.execute("INSERT INTO environ(timestamp, temperature, pressure, humidity, light, proximity, noise) \
                          VALUES('%s', %s, %s, %s, %s, %s, %s )" % 
                          (time, temp, pres, hum, light, prox, noise))
         self.conn.commit()
 
-    def add_gasses(self, time, CO, NO2, C2H5OH, H2, NH3, CO4, C3H8, C4H10)
-        self.cur.execute("INSERT INTO gasses(timestamp, CO2, NO2, C2H5OH, H2, NH3, CO4, C3H8, C4H10) 
+    def add_gasses(self, time, CO, NO2, C2H5OH, H2, NH3, CO4, C3H8, C4H10):
+        self.cur.execute("INSERT INTO gasses(timestamp, CO2, NO2, C2H5OH, H2, NH3, CO4, C3H8, C4H10) \
                          VALUES('%s', %s, %s, %s, %s, %s, %s, %s, %s )" % 
                          (time, CO, NO2, C2H5OH, H2, NH3, CO4, C3H8, C4H10))
         self.conn.commit()
 
-    def add_pms5003(self, time, pm1, pm25, pm10)
-        self.cur.execute("INSERT INTO pms5003(timestamp, pm1, pm25, pm10) 
+    def add_pms5003(self, time, pm1, pm25, pm10):
+        self.cur.execute("INSERT INTO pms5003(timestamp, pm1, pm25, pm10) \
                           VALUES('%s', %s, %s, %s)" % 
                           (time, pm1, pm25, pm10))
         self.conn.commit()
